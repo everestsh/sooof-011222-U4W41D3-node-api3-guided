d251f9ac48d1267d6bfb6a55a316e2ddec4e9f53
sooof <sooof@me.com>
2022-02-09 14:29:05 -0800

    iddleware #2 error test

--- api/server.js
+++ api/server.js
@@ -15,9 +15,10 @@ server.use((req, res, next)=>{
   // res.json('foobar')
 
   console.log('the req flowed through our custom middleware')
-  // res.set('X-Web-49', 'Rocks') // setting a header on the response
-  // res.set('Set-Cookie', 'foo=bar') // setting a cookie on the browser
+  res.set('X-Web-49', 'Rocks') // setting a header on the response
+  res.set('Set-Cookie', 'foo=bar') // setting a cookie on the browser
   next()
+  next({ status: 422, message: 'this is horrible'})
 })
 
 server.use('/api/hubs', hubsRouter);
@@ -34,5 +35,12 @@ server.use('*', (req, res) => {
   // catch all 404 errors middleware
   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  console.log('disaster!')
+  res.status(err.status || 500).json({
+    message: `The Horror: ${err.message}`,
+  })
+})
 
 module.exports = server;
